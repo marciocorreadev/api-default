db2238cb6e1c68c86eafd7696425f0390fbb38ca
Márcio Corrêa <msc@outlook.com.br>
2021-01-31 23:56:03 -0300

    first commit

--- src/routes/cest.ts
+++ src/routes/cest.ts
@@ -4,7 +4,6 @@ import AnyValueController from '@controllers/anyValue'
 const router = Router()
 router.get('/anyValue', AnyValueController.get)
 router.get('/anyValue/:anyValue', AnyValueController.getById)
-router.get('/anyValue/ncm/:ncm', AnyValueController.getByNcm)
 router.post('/anyValue', AnyValueController.create)
 router.put('/anyValue/:anyValue', AnyValueController.update)
 router.delete('/anyValue/:anyValue', AnyValueController.remove)
